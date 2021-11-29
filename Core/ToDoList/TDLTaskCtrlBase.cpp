6e352be059f022482e5ba0ee6f7d2e4caa650adf
abstractspoon <daniel.godson@gmail.com>
2021-11-29 18:09:23 +1100

    Fix date column width calculation

--- Core/ToDoList/TDLTaskCtrlBase.cpp
+++ Core/ToDoList/TDLTaskCtrlBase.cpp
@@ -683,6 +683,7 @@ void CTDLTaskCtrlBase::OnStylesUpdated(const CTDCStyleMap& styles, BOOL bAllowRe
 
 		case TDCS_SHOWWEEKDAYINDATES:
 			if (IsColumnShowing(TDCC_STARTDATE) ||
+				IsColumnShowing(TDCC_CREATIONDATE) ||
 				IsColumnShowing(TDCC_LASTMODDATE) ||
 				IsColumnShowing(TDCC_DUEDATE) ||
 				IsColumnShowing(TDCC_DONEDATE))
@@ -3389,8 +3390,9 @@ void CTDLTaskCtrlBase::DrawColumnDate(CDC* pDC, const COleDateTime& date, TDC_DA
 
 	int nSpace = pDC->GetTextExtent(_T(" ")).cx;
 
-	int nMaxDateWidth = (m_dateTimeWidths.nMaxDateWidth + nSpace);
-	int nMinDateWidth = (m_dateTimeWidths.nMinDateWidth + nSpace);
+	int nMaxDateWidth = m_dateTimeWidths.nMaxDateWidth;
+	int nMinDateWidth = m_dateTimeWidths.nMinDateWidth;
+
 	int nMaxTimeWidth = (bHasTime ? (m_dateTimeWidths.nMaxTimeWidth + nSpace) : 0);
 	int nMaxDayWidth = (bHasDow ? (m_dateTimeWidths.nMaxDowNameWidth + nSpace) : 0);
 
@@ -3449,7 +3451,7 @@ void CTDLTaskCtrlBase::DrawColumnDate(CDC* pDC, const COleDateTime& date, TDC_DA
 		// Sacrifice the date if it falls within 7 days
 		BOOL bWithin7Days = IsDateWithin7DaysOfToday(date, nDate);
 
-		nReqWidth = (nMaxDayWidth + nMaxTimeWidth);
+		nReqWidth = (nMaxDayWidth + nMaxTimeWidth - nSpace);
 
 		if (bHasDow && bWithin7Days && (nAvailWidth >= nReqWidth))
 		{
@@ -3482,7 +3484,7 @@ void CTDLTaskCtrlBase::DrawColumnDate(CDC* pDC, const COleDateTime& date, TDC_DA
 			bDrawTime = TRUE;
 			bDrawDate = FALSE;
 
-			nReqWidth = nMaxTimeWidth;
+			nReqWidth = nMaxTimeWidth - nSpace;
 			break;
 		}
 
@@ -3493,7 +3495,7 @@ void CTDLTaskCtrlBase::DrawColumnDate(CDC* pDC, const COleDateTime& date, TDC_DA
 			bDrawTime = FALSE;
 			bDrawDate = FALSE;
 
-			nReqWidth = nMaxDayWidth;
+			nReqWidth = nMaxDayWidth - nSpace;
 			break;
 		}
 
@@ -3564,7 +3566,7 @@ void CTDLTaskCtrlBase::DrawColumnDate(CDC* pDC, const COleDateTime& date, TDC_DA
 	if (bDrawDate)
 	{
 		DrawColumnText(pDC, sDate, rDraw, DT_RIGHT, crText);
-		rDraw.right -= nMaxDateWidth;
+		rDraw.right -= (nMaxDateWidth + nSpace);
 	}
 	
 	// Finally day of week
