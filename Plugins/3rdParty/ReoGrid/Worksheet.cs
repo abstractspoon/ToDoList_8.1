171eadf76286eb8fd4d2d74571e1f28142db51a2
abstractspoon <daniel.godson@gmail.com>
2022-06-23 16:07:30 +1000

    Remove commented out code

--- Plugins/3rdParty/ReoGrid/Worksheet.cs
+++ Plugins/3rdParty/ReoGrid/Worksheet.cs
@@ -433,9 +433,6 @@ namespace unvell.ReoGrid
 			this.FreezeToCell(row, col, FreezeArea.LeftTop);
 		}
 
-		//private CellPosition lastFrozenPosition;
-		//private FreezeArea lastFrozenArea = FreezeArea.None;
-
 		/// <summary>
 		/// Freezes worksheet at specified cell position and specifies the freeze areas.
 		/// </summary>
@@ -444,18 +441,6 @@ namespace unvell.ReoGrid
 		/// <param name="area">Specifies the frozen panes.</param>
 		public void FreezeToCell(int row, int col, FreezeArea area)
 		{
-			/////////////////////////////////////////////////////////////////
-			// fix issue #151, #172, #313
-			//if (lastFrozenPosition == new CellPosition(row, col) && lastFrozenArea == area)
-			//{
-			//	//skip to perform freeze if forzen position and area are not changed
-			//	return;
-			//}
-
-			//lastFrozenPosition = new CellPosition(row, col);
-			//lastFrozenArea = area;
-			/////////////////////////////////////////////////////////////////
-
 			if (this.viewportController != null)
 			{
 				// update viewport bounds - sometimes the viewport may cannot get the correct size for freezing,
