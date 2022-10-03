9668e0ae18ea28766309078cb9da4f9b9a17c51f
abstractspoon <daniel.godson@gmail.com>
2022-10-03 19:49:19 +1100

    Change error prompt button from 'OK' to 'Retry'

--- Core/ToDoList/ToDoListWnd.cpp
+++ Core/ToDoList/ToDoListWnd.cpp
@@ -10036,7 +10036,7 @@ BOOL CToDoListWnd::CanOverwriteExportFiles(const CStringArray& aExportPaths) con
 
 	while (FileMisc::GetNonWritableFiles(aExistPaths, aLockedPaths))
 	{
-		if (IDCANCEL == CMessageBox::AfxShow(IDS_ERROR_LOCKEDFILES, Misc::FormatArray(aLockedPaths, _T("\n\n")), MB_ICONERROR | MB_OKCANCEL))
+		if (IDCANCEL == CMessageBox::AfxShow(IDS_ERROR_LOCKEDFILES, Misc::FormatArray(aLockedPaths, _T("\n\n")), MB_ICONERROR | MB_RETRYCANCEL))
 			return FALSE;
 	}
 
@@ -12173,7 +12173,7 @@ void CToDoListWnd::OnToolsAnalyseLoggedTime()
 
 				while (FileMisc::GetNonWritableFiles(aLogFiles, aLockedPaths))
 				{
-					if (IDCANCEL == CMessageBox::AfxShow(IDS_ERROR_LOCKEDFILES, Misc::FormatArray(aLockedPaths, _T("\n\n")), MB_ICONERROR | MB_OKCANCEL))
+					if (IDCANCEL == CMessageBox::AfxShow(IDS_ERROR_LOCKEDFILES, Misc::FormatArray(aLockedPaths, _T("\n\n")), MB_ICONERROR | MB_RETRYCANCEL))
 						return;
 				}
 
